6c6ced4bf663350fcf3a15941c2b573ffe645315
alvinchengyi <alvin.cheng@yale.edu>
2016-02-02 22:51:39 +0000

    Parse submitted forms

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
+var bodyParser = require('body-parser');
 
-// set tamplate
+// configure our app
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-//hello world
+
+//main page
 app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//register page
+
+app.post('/user/register', function (req, res) {
+  res.send(req.body);
+  console.log('The user has the email address', req.body.email);
 });
 
+//receive message from server
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT);
 });
