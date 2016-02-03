69257e18fe4600906ff0ad088af3d44ac6d7cc53
alvinchengyi <alvin.cheng@yale.edu>
2016-02-03 03:30:20 +0000

    session handling

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ var exphbs = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
 var Users = require('./models/users.js');
+var session = require('express-session');
 
 // configure our app
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: 'auto' }
+}))
+// session handling
+
+app.use(function(req, res, next){
+  console.log('req.session =', req.session);
+  if(req.session.userId){
+    Users.findById(req.session.userId, function(err, user){
+      if(!err){
+        res.locals.currentUser = user;
+      }
+      next();
+    })
+  }else{
+    next();
+  }
+})
 
 //main page funtions
 
@@ -17,29 +39,32 @@ app.get('/', function (req, res) {
     if (err) {
       res.send('error getting users');
     }else{
-      res.render('index', {userCount: users.length});
+      res.render('index', {
+        userCount: users.length,
+        currentUser: res.locals.currentUser
+      });
     }
   });
 });
 
-//main page funtions
+//register validation check and feedback
 
 app.post('/user/register', function (req, res) {
    if(req.body.password !== req.body.password_confirmation){
       return res.render('index', {errors: "Password and password confirmation do not match"});
   }
-  
   var newUser = new Users();
   newUser.hashed_password = req.body.password;
   newUser.email = req.body.email;
   newUser.name = req.body.fl_name;
-  newUser.save(function(err){
+  newUser.save(function(err, user){
+    req.session.userId = user._id;
     if(err){
       res.render('index', {errors: err});
     }else{
       res.redirect('/');
     }
-  })
+  });
   console.log('The user has the email address', req.body.email);
 });
 
